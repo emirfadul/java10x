ed6c3fe654084be159ae0e7f0994056f2456be5f
Emir Fadul <emirfadul@gmail.com>
2025-12-16 12:31:05 -0400

    atualizado em 16.dez as 12:34

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -4,13 +4,17 @@ public class Array {
     static void main(String[] args) {
 
         String[] ninja = new String[4];
-
         ninja[0] = "naruto";
         ninja[1] = "sasuke";
         ninja[2] = "sakura";
         ninja[3] = "hinata";
 
         System.out.println(ninja[3]);
+        //System.out.println(ninja);
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(ninja[i]);
+        }
 
 
 
---
+++ src/Desafio/Desafio2.java
@@ -0,0 +1,29 @@
+package Desafio;
+
+import java.util.Scanner;
+
+public class Desafio2 {
+
+    static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroMax = 10;
+        String[] ninjas = new String[numeroMax];
+
+        int cadastrados = 0;
+        int opcoes = 0;
+
+        System.out.println("\n====== Menu Ninja =========");
+        System.out.println("1. Cadastrar Ninja");
+        System.out.println("\n====== Menu Ninja =========");
+        System.out.println("\n====== Menu Ninja =========");
+        System.out.println("\n====== Menu Ninja =========");
+
+
+
+
+
+
+    }
+}
