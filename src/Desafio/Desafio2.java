dd65d73dd47e3db9142cec2df84b165b060da81a
emirfadul <emirfadul@emirfadul3.local>
2025-12-17 12:22:13 -0400

    atualizado em 17.dez as 12:22

--- src/Desafio/Desafio2.java
+++ src/Desafio/Desafio2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Desafio2 {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
@@ -14,16 +14,52 @@ public class Desafio2 {
         int cadastrados = 0;
         int opcoes = 0;
 
-        System.out.println("\n====== Menu Ninja =========");
-        System.out.println("1. Cadastrar Ninja");
-        System.out.println("\n====== Menu Ninja =========");
-        System.out.println("\n====== Menu Ninja =========");
-        System.out.println("\n====== Menu Ninja =========");
+        while (opcoes != 3) {
 
 
+            System.out.println("\n====== Menu Ninja =========");
+            System.out.println("1. Cadastrar");
+            System.out.println("2. Listar");
+            System.out.println("3. Sair");
+            System.out.println("Escolha uma Opção: ");
+            opcoes = scanner.nextInt();
+
+            switch (opcoes) {
+                case 1:
+                    if(cadastrados < numeroMax) {
+                        System.out.println("Digite o nome do ninja: ");
+                        String nome = scanner.next();
+                        ninjas[cadastrados] = nome;
+                        cadastrados++;
+                        System.out.println("Ninja cadastrado com sucesso!");
+                        break;
+                    } else {
+                        System.out.println("Lista está cheia");                        
+                    }
+                    break;
+
+                case 2:
+                    if(cadastrados > 0) {
+                        System.out.println("Ninjas cadastrados:");
+                        for (int i = 0; i < cadastrados; i++) {
+                            System.out.println((i+1) + ". " + ninjas[i]);
+                        }
+                    } else {
+                        System.out.println("Nenhum ninja cadastrado");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Fim do acesso");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }            
 
 
-
-
+        }
+        scanner.close();
     }
 }
